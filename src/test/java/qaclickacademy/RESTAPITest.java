dcaf8cd4c0b68013bbbd203f6d34b139ce427cd4
user <gadwan@student.uls.edu.lb>
2022-04-26 12:30:06 +0300

    American gut commit

--- src/test/java/qaclickacademy/RESTAPITest.java
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -10,6 +10,16 @@ public class RESTAPITest {
 		System.out.println("postJira");
 		System.out.println("postJira2");
 		System.out.println("postJira3");
+		//R
+		//O
+		//C
+		//K
+		//E
+		//T
+		System.out.println("postJira4");
+		System.out.println("postJira5");
+		System.out.println("postJira6");
+		System.out.println("postJira7");
 	}
 	
 	@Test
